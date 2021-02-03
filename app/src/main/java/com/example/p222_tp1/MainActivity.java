e682f905a74eb82ab20747d38465ec38489f4696
Dorian BLATIERE <dorian.blatiere@hotmail.fr>
2021-02-03 09:13:03 +0100

    second commit

--- app/src/main/java/com/example/p222_tp1/MainActivity.java
+++ app/src/main/java/com/example/p222_tp1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.p222_tp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Bonjour!!!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
