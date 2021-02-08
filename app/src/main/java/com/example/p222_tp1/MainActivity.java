e1e520ee57fe662ba1e15c937f9633510ce3bdc5
Dorian BLATIERE <dorian.blatiere@hotmail.fr>
2021-02-08 11:42:29 +0100

    8/02/2021 commit1

--- app/src/main/java/com/example/p222_tp1/MainActivity.java
+++ app/src/main/java/com/example/p222_tp1/MainActivity.java
@@ -4,18 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity:LOG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, R.string.bonjour, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, R.string.textToastBonjour, Toast.LENGTH_SHORT).show();
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), R.string.textToastClicButton, Toast.LENGTH_SHORT).show();
+
+                // Affichage d'un message verbose dans le log
+                Log.v(MainActivity.TAG, "Clic");
+            }
+        });
 
 
 
     }
+
 }
